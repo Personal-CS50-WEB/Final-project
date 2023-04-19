5d4c9a6651fcaebaf26e64f0b5fbb47ea18ab031
Olamakhlouf <olamoatasem@gmail.com>
2023-04-19 12:45:39 -0300

    requirments.txt for eb5

--- final/settings.py
+++ final/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import environ
 from datetime import timedelta  
 import dj_database_url
-import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
