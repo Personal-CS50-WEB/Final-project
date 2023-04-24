11fc7da306a8eab50f984416b5927beecb15c65c
Olamakhlouf <olamoatasem@gmail.com>
2023-04-24 14:45:54 -0300

    secure

--- final/settings.py
+++ final/settings.py
@@ -134,6 +134,8 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
